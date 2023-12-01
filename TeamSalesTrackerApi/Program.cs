417277f0afed9caa6ac6ddb96bc04cd0190db9b6
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2023-12-01 19:13:02 -0300

    Dependencies

--- TeamSalesTrackerApi/Program.cs
+++ TeamSalesTrackerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    //Titulo (Diseño)
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Team Sales Tracker API", Version = "v1" });
+
+    //Boton autorizar
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference{
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{ }
+        }
+    });
+});
+//builder.Services.AddDbContext<>(options =>
+//{
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+//});
+
 
 var app = builder.Build();
 
